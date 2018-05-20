5d9e536bd114a2628b625e86e16e6a1291bec2d9
RNogales <rnogales@correo.ugr.es>
2018-05-20 10:21:06 +0200

    add Google modules

--- index.js
+++ index.js
@@ -1,6 +1,11 @@
 var express = require('express');
 var app = express();
 var port = process.env.PORT || 3000;
+const {OAuth2Client} = require('google-auth-library');
+const googleId = "806589839267-n7f2r90tj06pdjd479r0dbh8m1m1poo4.apps.googleusercontent.com";
+const auth = new OAuth2Client(googleId);
+const { Pool } = require('pg');
+const bodyParser = require('body-parser');
 
 app.get('/', function (req, res) {
   res.send('Holaaa bonicaa!');
