73845b8514e36b95d34369c6050ba42a39f834ac
RNogales <rnogales@correo.ugr.es>
2018-05-20 10:32:51 +0200

    edit port

--- index.js
+++ index.js
@@ -81,5 +81,5 @@ app.get('/username', getUserid, function(request, response) {
 });
 
 app.listen(app.get('port'), function() {
-	console.log('Node app is running on port', app.get('port'));
+	console.log('Node app is running on port: '+port;
 });
